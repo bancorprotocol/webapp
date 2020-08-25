bb3919a1c1ce51850a4f141bc4d9328ce07be6df
Jan Langheimer <langheimer@gmail.com>
2020-08-25 14:05:49 +0200

    extract data view into component

---
+++ src/components/data/DataAll.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-row>
+    <b-col cols="12">
+      <content-block title="Statistics" :shadow-light="true">
+        <statistics class="mt-3" />
+      </content-block>
+    </b-col>
+    <b-col v-if="false" md="6">
+      <content-block title="Liquidity">
+        <liquidity-chart />
+      </content-block>
+    </b-col>
+    <b-col v-if="false" md="6">
+      <content-block title="Volume">
+        <liquidity-chart />
+      </content-block>
+    </b-col>
+    <b-col>
+      <content-block :px0="true" :shadow-light="true" :no-header="true">
+        <data-table />
+      </content-block>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { vxm } from "@/store";
+import ContentBlock from "@/components/common/ContentBlock.vue";
+import Statistics from "@/components/data/statistics/Statistics.vue";
+import LiquidityChart from "@/components/data/charts/LiquidityChart.vue";
+import DataTable from "@/components/data/data-tables/DataTables.vue";
+
+@Component({
+  components: {
+    LiquidityChart,
+    Statistics,
+    ContentBlock,
+    DataTable
+  }
+})
+export default class DataAll extends Vue {
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+}
+</script>
+
+<style lang="scss"></style>
