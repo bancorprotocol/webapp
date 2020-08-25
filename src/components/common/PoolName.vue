49d1f416288bb235bfdcf4370ebd666ad10b8d43
Jan Langheimer <langheimer@gmail.com>
2020-08-25 18:45:01 +0200

    tmp commit

---
+++ src/components/common/PoolName.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span v-for="(reserve, index) in pool.reserves" :key="reserve.id">
+      {{ reserve.symbol }}
+      <span v-if="index + 1 < pool.reserves.length">/</span>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { ViewRelay } from "@/types/bancor";
+import { vxm } from "@/store";
+
+@Component({
+  components: {}
+})
+export default class PoolLogos extends Vue {
+  @Prop() pool!: ViewRelay;
+
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/components/data/details/DataPool.vue
+++ src/components/data/details/DataPool.vue
@@ -1,16 +1,37 @@
 <template>
   <b-row>
+    <b-col cols="6">
+      <pool-logos
+        class="my-1"
+        :pool="pool"
+        :cursor="false"
+        :version="false"
+        :showName="false"
+      />
+      <div
+        class="font-size-20 font-w600 mt-2 mb-3 d-flex"
+        :class="[darkMode ? 'text-dark' : 'text-light']"
+      >
+        <pool-name :pool="pool" class="mr-2" />
+        Pool
+      </div>
+    </b-col>
+    <b-col cols="6"> </b-col>
     <b-col cols="4">
-      <content-block title="Statistics" :shadow-light="true">
+      <content-block title="Pool Tokens Statistics" :shadow-light="true">
         <statistics class="mt-3" />
       </content-block>
 
-      <content-block title="Statistics" :shadow-light="true">
-        <statistics class="mt-3" />
+      <content-block title="Pool Information" :shadow-light="true">
+        <div class="mt-3">
+          <label-content-split label="Pool Name" value="123" />
+          <label-content-split label="Pool Address" :value="pool.id" />
+          <label-content-split label="Pool Name" value="123" />
+        </div>
       </content-block>
     </b-col>
     <b-col md="8">
-      <content-block title="Liquidity">
+      <content-block title="Liquidity" :shadow-light="true">
         <liquidity-chart />
       </content-block>
     </b-col>
@@ -30,13 +51,19 @@ import Statistics from "@/components/data/statistics/Statistics.vue";
 import LiquidityChart from "@/components/data/charts/LiquidityChart.vue";
 import DataTable from "@/components/data/data-tables/DataTables.vue";
 import { ViewToken, ViewReserve } from "@/types/bancor";
+import PoolLogos from "@/components/common/PoolLogos.vue";
+import PoolName from "@/components/common/PoolName.vue";
+import LabelContentSplit from "@/components/common/LabelContentSplit.vue";
 
 @Component({
   components: {
     LiquidityChart,
     Statistics,
     ContentBlock,
-    DataTable
+    DataTable,
+    PoolLogos,
+    PoolName,
+    LabelContentSplit
   }
 })
 export default class DataPool extends Vue {
