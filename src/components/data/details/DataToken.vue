92ae67d3c3ba4b930ab04a3c13cd1c48b0235b05
Jan Langheimer <langheimer@gmail.com>
2020-08-25 14:36:24 +0200

    create Data Details Pool view component

---
+++ src/components/data/details/DataToken.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-row>
+    <b-col cols="4">
+      <content-block title="Statistics" :shadow-light="true">
+        <statistics class="mt-3" />
+      </content-block>
+
+      <content-block title="Statistics" :shadow-light="true">
+        <statistics class="mt-3" />
+      </content-block>
+    </b-col>
+    <b-col md="8">
+      <content-block title="Liquidity">
+        <liquidity-chart />
+      </content-block>
+    </b-col>
+    <b-col>
+      <content-block :px0="true" :shadow-light="true" :no-header="true">
+        <data-table />
+      </content-block>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { vxm } from "@/store";
+import ContentBlock from "@/components/common/ContentBlock.vue";
+import Statistics from "@/components/data/statistics/Statistics.vue";
+import LiquidityChart from "@/components/data/charts/LiquidityChart.vue";
+import DataTable from "@/components/data/data-tables/DataTables.vue";
+import { ViewToken, ViewReserve } from "@/types/bancor";
+
+@Component({
+  components: {
+    LiquidityChart,
+    Statistics,
+    ContentBlock,
+    DataTable
+  }
+})
+export default class DataToken extends Vue {
+  token!: ViewToken;
+
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+
+  created() {
+    const id = this.$route.params.id;
+    const token = vxm.bancor.token(id);
+    if (token) this.token = token;
+    else this.$router.replace({ name: "404" });
+  }
+}
+</script>
+
+<style lang="scss"></style>
