39b376e4315f90cdd3a70f124f0f7e356993e8ac
John Williamson <johnjwilliamson@live.com>
2020-09-09 11:39:57 +1000

    render new statistics

--- src/components/data/statistics/Statistics.vue
+++ src/components/data/statistics/Statistics.vue
@@ -10,13 +10,15 @@
     <b-col md="6" lg="3" class="mb-0">
       <statistics-data-block title="Total Liquidity" :value="liquidityDepth" />
     </b-col>
-    <!-- <b-col md="6" lg="3" class="mb-4 mb-md-0 mb-lg-0"> -->
-    <!-- <statistics-data-block -->
-    <!-- title="24hrs Volume" -->
-    <!-- value="123,456,789.00 ETH" -->
-    <!-- :percentage="-0.89" -->
-    <!-- /> -->
-    <!-- </b-col> -->
+    <b-col md="6" lg="3" class="mb-4 mb-md-0 mb-lg-0">
+      <statistics-data-block title="BNT Price" :value="bntPrice" />
+    </b-col>
+    <b-col md="6" lg="3" class="mb-4 mb-md-0 mb-lg-0">
+      <statistics-data-block
+        title="Recent Conversions (24hrs)"
+        :value="twentyFourHourTradeCount"
+      />
+    </b-col>
     <!-- <b-col md="6" lg="3"> -->
     <!-- <statistics-data-block -->
     <!-- title="24hrs Transactions" -->
@@ -37,10 +39,16 @@ import numeral from "numeral";
   components: { StatisticsDataBlock }
 })
 export default class Statistics extends Vue {
-  numeral = numeral;
-
   get liquidityDepth() {
     return numeral(this.stats.totalLiquidityDepth).format("$0,0.00");
+  }
+
+  get bntPrice() {
+    return numeral(this.stats.bntPrice).format("$0,0.00");
+  }
+
+  get twentyFourHourTradeCount() {
+    return String(this.stats.twentyFourHourTradeCount);
   }
 
   get stats() {
