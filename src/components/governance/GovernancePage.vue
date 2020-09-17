c460472f4f941d03f7bde68338e5add522cb47ab
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-09-17 12:03:36 +0200

    Create clean Proposals block

--- src/components/governance/GovernancePage.vue
+++ src/components/governance/GovernancePage.vue
@@ -1,17 +1,24 @@
 <template>
-  <b-col cols="4">
-    <stake />
-  </b-col>
+  <b-row>
+    <b-col cols="4">
+      <stake />
+    </b-col>
+    <b-col cols="8">
+      <proposals />
+    </b-col>
+  </b-row>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { vxm } from "@/store";
 import Stake from "@/components/governance/stake/Stake.vue";
+import Proposals from "@/components/governance/stake/Proposals.vue";
 
 @Component({
   components: {
-    Stake
+    Stake,
+    Proposals,
   }
 })
 export default class GovernancePage extends Vue {
---
+++ src/components/governance/stake/Proposals.vue
@@ -0,0 +1,40 @@
+<template>
+  <content-block title="Proposals" :shadow-light="true">
+    <div class="pt-2">
+      <div class="float-right mr-2">
+        + add
+      </div>
+      <b-tabs no-fade :class="darkMode ? 'tabs-dark' : 'tabs-light'">
+        <b-tab title="Open" active>
+          Open
+        </b-tab>
+        <b-tab title="Done">
+          Done
+        </b-tab>
+      </b-tabs>
+    </div>
+  </content-block>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { vxm } from "@/store";
+import ContentBlock from "@/components/common/ContentBlock.vue";
+
+@Component({
+  components: {
+    ContentBlock,
+  }
+})
+export default class Stake extends Vue {
+  get isEth() {
+    return this.$route.params.service === "eth";
+  }
+
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+}
+</script>
+
+<style lang="scss"></style>
