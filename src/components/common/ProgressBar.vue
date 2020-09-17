6427b8b399227bad09e8e4d2babb2d6402c920c0
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-09-17 15:13:26 +0200

    Initial progress bar component

---
+++ src/components/common/ProgressBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="`progress-line-bar progress-line-bar--${type}`">
+    <div class="progress-line-bar__progress fade" :style="{width: percentageValue}"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { vxm } from "@/store";
+import numeral from "numeral";
+
+@Component
+export default class ProgressBar extends Vue {
+  @Prop() type?: 'warn' | 'error' | 'info';
+  @Prop() percentage?: number;
+
+  get percentageValue() {
+    return `${(+this.percentage)?.toFixed(2)}%`
+  }
+
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/_scss/custom/_variables";
+
+$progress-line-bar---height: 4px;
+$progress-line-bar---border-radius: 0;
+
+$progress-line-bar---background: $gray-border;
+$progress-line-bar__progress---background: $primary;
+$progress-line-bar--warn__progress---background: $text-warning-light;
+$progress-line-bar--error__progress---background: $text-error-light;
+$progress-line-bar--info__progress---background: #3ec8c8;
+
+.progress-line-bar {
+  border-radius: $progress-line-bar---border-radius;
+  overflow: hidden;
+  position: relative;
+  background: $progress-line-bar---background;
+  
+  &,
+  &__progress {
+    height: $progress-line-bar---height;
+  }
+
+  &__progress {
+    background: $progress-line-bar__progress---background;
+  }
+
+  &--info &__progress {
+    background: $progress-line-bar--info__progress---background;
+  }
+  &--warn &__progress {
+    background: $progress-line-bar--warn__progress---background;
+  }
+  &--error &__progress {
+    background: $progress-line-bar--error__progress---background;
+  }
+}
+</style>
