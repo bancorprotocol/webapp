950244f00229908d60a988beab6176ac8c77cabd
Jan Langheimer <langheimer@gmail.com>
2020-09-18 12:52:16 +0200

    add tokens details view

--- src/components/data/details/DataDetailsToken.vue
+++ src/components/data/details/DataDetailsToken.vue
@@ -2,13 +2,16 @@
   <div v-if="!loading">
     <b-row class="mb-3">
       <b-col cols="6">
-        <pool-logos-overlapped :pool-id="pool.id" size="48" />
+        <img
+          class="img-avatar img-avatar48 bg-white logo-shadow"
+          :src="token.logo"
+          alt="Token Logo"
+        />
         <div
           class="font-size-20 font-w600 mt-3 d-flex align-items-center"
           :class="[darkMode ? 'text-dark' : 'text-light']"
         >
-          <span class="mr-2">{{ poolName }} Pool</span>
-          <version-badge :version="poolVersion" />
+          <span class="mr-2">{{ token.symbol }} Token</span>
         </div>
       </b-col>
 
@@ -30,7 +33,7 @@
 
     <b-row>
       <b-col cols="4">
-        <content-block title="Pool Tokens Statistics" :shadow-light="true">
+        <content-block title="Token Statistics" :shadow-light="true">
           <b-row class="mt-3">
             <b-col cols="6">
               <statistics-data-block
@@ -53,18 +56,11 @@
           </b-row>
         </content-block>
 
-        <content-block title="Pool Information" :shadow-light="true">
+        <content-block title="Token Information" :shadow-light="true">
           <div class="my-3">
-            <label-content-split label="Pool Name" :value="poolName" />
-            <label-content-split
-              label="Pool Address"
-              :value="shortAddress"
-              class="my-3"
-            />
-            <label-content-split
-              label="Total Pool Weight"
-              :value="totalPoolWeight"
-            />
+            <label-content-split label="Symbol" :value="token.symbol" />
+            <label-content-split label="Name" :value="tokenName" class="my-3" />
+            <label-content-split label="Token Address" :value="shortAddress" />
           </div>
 
           <main-button label="View on Block Explorer" class="font-size-14" />
@@ -79,19 +75,10 @@
         <content-block
           :px0="true"
           :shadow-light="true"
-          title="Pool Tokens"
+          title="Token Pools"
           :search.sync="searchTokens"
         >
-          <table-wrapper
-            :items="poolTokensTable.items"
-            :fields="poolTokensTable.fields"
-            :filter="searchTokens"
-            sort-by="reserveWeight"
-          >
-            <template v-slot:cell(symbol)="data">
-              <pool-logos :token="data.item" :cursor="false" />
-            </template>
-          </table-wrapper>
+          <table-pools :items="poolTableItems" :filter="searchTokens" />
         </content-block>
       </b-col>
 
@@ -116,7 +103,7 @@ import { Component, Vue } from "vue-property-decorator";
 import { vxm } from "@/store";
 import ContentBlock from "@/components/common/ContentBlock.vue";
 import Statistics from "@/components/data/statistics/Statistics.vue";
-import { ViewRelay } from "@/types/bancor";
+import {ViewRelay, ViewReserve, ViewToken} from "@/types/bancor";
 import PoolLogos from "@/components/common/PoolLogos.vue";
 import LabelContentSplit from "@/components/common/LabelContentSplit.vue";
 import StatisticsDataBlock from "@/components/data/statistics/StatisticsDataBlock.vue";
@@ -128,6 +115,7 @@ import VersionBadge from "@/components/common/VersionBadge.vue";
 import TableWrapper from "@/components/common/TableWrapper.vue";
 import numeral from "numeral";
 import TableTransactions from "@/components/data/transactiontables/TableTransactions.vue";
+import TablePools from "@/components/data/pooltokentables/TablePools.vue";
 export interface TxHistoryView {
   addEvents: any[];
   conversionEvents: any[];
@@ -136,6 +124,7 @@ export interface TxHistoryView {
 
 @Component({
   components: {
+    TablePools,
     TableTransactions,
     TableWrapper,
     VersionBadge,
@@ -149,7 +138,7 @@ export interface TxHistoryView {
     MainButton
   }
 })
-export default class DataPool extends Vue {
+export default class DataDetailsToken extends Vue {
   loadingFocusPool = false;
   searchTx = "";
   searchTokens = "";
@@ -160,7 +149,7 @@ export default class DataPool extends Vue {
     removeEvents: []
   };
 
-  get poolId() {
+  get tokenId() {
     return this.$route.params.id;
   }
 
@@ -168,19 +157,19 @@ export default class DataPool extends Vue {
     return vxm.bancor.loadingTokens;
   }
 
-  get pool(): ViewRelay {
-    return vxm.bancor.relay(this.poolId);
+  get token(): ViewToken {
+    return vxm.bancor.token(this.tokenId);
   }
 
   get liqDepth() {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD"
-    }).format(this.pool.liqDepth);
+    }).format(this.token.liqDepth ?? 0);
   }
 
   get shortAddress() {
-    return shortenEthAddress(this.pool.id);
+    return shortenEthAddress(this.token.contract);
   }
 
   get volume24h() {
@@ -195,65 +184,16 @@ export default class DataPool extends Vue {
     return "????????";
   }
 
-  get poolName() {
-    return buildPoolName(this.pool.id);
-  }
-
-  get totalPoolWeight() {
-    const weights = this.poolTokensTable.items.map(x => x.reserveWeight)
-    const total = weights.reduce((accumulator, currentValue) => accumulator + currentValue)
-    return numeral(total).format('0%');
-  }
-
-  get poolVersion() {
-    return this.pool.v2 ? 2 : 1;
+  get tokenName() {
+    return this.token.name;
   }
 
   get darkMode() {
     return vxm.general.darkMode;
   }
 
-  get poolTokensTable() {
-    const items = this.pool.reserves
-
-    const fields: any[] = [
-      {
-        key: "reserveWeight",
-        label: "Token Weight",
-        sortable: true,
-        formatter: (value: string) => numeral(value).format("0%")
-      },
-      {
-        key: "symbol",
-        label: "Token Name",
-        sortable: true
-      },
-      {
-        key: "contract",
-        label: "Token Address",
-        sortable: true,
-        formatter: (value: string) => shortenEthAddress(value)
-      },
-      {
-        key: "pooledTokens",
-        label: "Pooled Tokens",
-        sortable: true,
-        formatter: (value: any, label: string, item: any) =>
-          `${formatNumber(value ?? 0)} ${item.symbol}`
-      },
-      {
-        key: "usdValue",
-        label: "USD Value",
-        sortable: true,
-        formatter: (value: number) =>
-          new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-          }).format(value ?? 0)
-      }
-    ];
-
-    return { items, fields };
+  get poolTableItems() {
+    return vxm.bancor.relays.filter((x: ViewRelay) => x.reserves.find((x: ViewReserve) => x.symbol === this.token.symbol));
   }
 
   get txItemsSwap() {
@@ -263,7 +203,7 @@ export default class DataPool extends Vue {
   async loadFocusPool() {
     this.loadingFocusPool = true;
     try {
-      this.txHistory = await vxm.bancor.focusPool(this.poolId);
+      this.txHistory = await vxm.bancor.focusSymbol(this.token.symbol);
     } catch (e) {
       console.log(e);
     } finally {
@@ -272,9 +212,16 @@ export default class DataPool extends Vue {
   }
 
   async created() {
-    await this.loadFocusPool();
+    // await this.loadFocusPool();
   }
 }
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+@import "../../../assets/_scss/custom/variables";
+
+.logo-shadow {
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+  border: solid 1px $gray-border;
+}
+</style>
