abd9f91449c51f3249ac4829b4d2cb173191deae
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-09-22 13:36:55 +0200

    Add a initial stake input

--- src/components/modals/ModalStake.vue
+++ src/components/modals/ModalStake.vue
@@ -31,7 +31,22 @@
     </template>
 
     <div>
-      CONTENT
+      <div class="font-size-12 font-w500" :class="darkMode ? 'text-muted-dark' : 'text-muted-light'">
+        <span class="text-uppercase">Stake your tokens</span>
+        <span class="float-right">Balance: {{120}} gBTN</span>
+      </div>
+
+      <div class="input-currency mt-1">
+        <b-form-input type="number" size="lg" class="input-currency__input"></b-form-input>
+        <div class="input-currency__append pr-3">
+          <img
+            class="img-avatar img-avatar32 bg-dark input-currency__img mr-2 ml-3"
+            src="@/assets/media/logos/bancor-white2.png">
+
+          <span class=" font-size-14 font-w500">gBNT</span>
+        </div>
+        
+      </div>
     </div>
   </b-modal>
 </template>
@@ -66,5 +81,33 @@ export default class ModalBase extends Vue {
 <style lang="scss">
 .modal-body {
   padding-top: 0 !important;
+}
+.input-currency {
+  border-right: 0 !important;
+  position: relative;
+
+  &__append {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    height: 100%;
+    right: 0;
+  }
+  &__img {
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+    border: solid 1px #e6ebf2;
+    padding: 4px;
+  }
+  &__input {
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    &[type=number] {
+      -moz-appearance: textfield;
+    }
+  }
 }
 </style>
