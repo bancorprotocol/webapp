b4444c5899a8ec5de8d718110732b2b9853f448b
Jan Langheimer <langheimer@gmail.com>
2020-09-25 17:33:05 +0200

    add collapseable block component

---
+++ src/components/common/CollapsableBlock.vue
@@ -0,0 +1,73 @@
+<template>
+  <b-card header-tag="header" no-body>
+    <template v-slot:header>
+      <div
+        @click="open = !open"
+        class="block-header d-flex justify-content-between font-size-14 font-w600 cursor"
+      >
+        <span class="my-1" :class="darkMode ? 'text-dark' : 'text-light'">
+          <font-awesome-icon
+            v-if="headerIcon"
+            :icon="headerIcon"
+            class="mr-1"
+            :class="darkMode ? 'text-white' : 'text-primary'"
+          />
+          {{ title }}
+        </span>
+
+        <font-awesome-icon
+          :icon="open ? 'caret-up' : 'caret-down'"
+          :class="darkMode ? 'text-white' : 'text-primary'"
+        />
+      </div>
+    </template>
+
+    <b-collapse id="collapse-block" v-model="open">
+      <slot></slot>
+    </b-collapse>
+  </b-card>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { vxm } from "@/store/";
+
+@Component({
+  components: {}
+})
+export default class CollapsableBlock extends Vue {
+  @Prop() title!: string;
+  @Prop({ default: "" }) headerIcon!: string;
+
+  open = false;
+
+  get darkMode() {
+    return vxm.general.darkMode;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/_scss/custom/_variables";
+.card {
+  border: 1px solid #e7ebf1 !important;
+  border-radius: 8px !important;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.04),
+    0 1px 2px 0 rgba(15, 89, 209, 0.08) !important;
+  background-color: #f7f9fc !important;
+}
+.card-header {
+  padding: 0 !important;
+  margin-bottom: 0 !important;
+  background-color: #e7ebf1 !important;
+  border-bottom: none !important;
+  border-radius: 8px 8px !important;
+}
+
+.border-bottom-light {
+  border-bottom: 1px solid $gray-border;
+}
+
+.border-bottom-dark {
+  border-bottom: 1px solid $gray-border-dark;
+}
+</style>
