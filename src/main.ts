4ac5a9e343c21fb39ef982e4cfa7a89e73de287a
John Williamson <johnjwilliamson@live.com>
2020-11-02 18:17:55 +1000

    adjust sample rate

--- src/main.ts
+++ src/main.ts
@@ -40,10 +40,8 @@ Sentry.init({
     }),
     new Integrations.BrowserTracing()
   ],
-
-  //   // We recommend adjusting this value in production, or using tracesSampler
-  //   // for finer control
-  tracesSampleRate: 1.0
+  sampleRate: 0.1,
+  tracesSampleRate: 0.1
 });
 
 const firebaseConfig = {
