bb894c2dea528e1f5bc7b089ce627613d2f9a996
Will Park <korea651@hanmail.net>
2020-11-05 02:32:11 +0900

    Update version of cli-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,26 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  moduleFileExtensions: ["js", "ts", "json", "vue"],
-  transform: { "^.+\\.(ts|tsx)$": "ts-jest" }
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  
+  // preset: "@vue/cli-plugin-unit-jest",
+  // moduleFileExtensions: ["js", "ts", "json", "vue", "node"],
+  // transform: { 
+  //   '^.+\\.vue$': 'vue-jest',
+  //   '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+  //     'jest-transform-stub',
+  //   "^.+\\.(ts|tsx)$": "ts-jest",
+  //   "^.+\\.js$": "babel-jest"
+  // },
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/src/$1'
+  // }, 
+  // globals: {
+  //   'ts-jest': {
+  //     babelConfig: true
+  //   }
+  // },
+  transformIgnorePatterns: ['node_modules']
+
+  // transformIgnorePatterns: [
+  //   '/node_modules/(?!eos-transit-lynx-provider)'    
+  // ]
 };
