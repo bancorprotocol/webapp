a685ddbbddb7a295ba55a07b6f1a2f35b907cea0
Jan Langheimer <langheimer@gmail.com>
2020-11-05 12:56:05 +0100

    remove TableWrapper.vue component

--- src/components/data/pooltokentables/TablePools.vue
+++ src/components/data/pooltokentables/TablePools.vue
@@ -1,72 +1,6 @@
 <template>
-  <table-wrapper
-    v-if="false"
-    primarykey="id"
-    :items="items"
-    :fields="fields"
-    :filter="filter"
-    :filterFunction="doFilter"
-    sort-by="liqDepth"
-  >
-    <template v-slot:head(liqDepth)="data">
-      {{ data.label }}
-      <font-awesome-icon
-        v-b-popover.hover.top="toolTips.liqDepth"
-        icon="info-circle"
-      />
-    </template>
-
-    <template v-slot:head(fee)="data">
-      {{ data.label }}
-      <font-awesome-icon
-        v-b-popover.hover.top="toolTips.fee"
-        icon="info-circle"
-      />
-    </template>
-
-    <template v-slot:head(feesGenerated)="data">
-      {{ data.label }}
-      <font-awesome-icon
-        v-b-popover.hover.top="toolTips.feesGenerated"
-        icon="info-circle"
-      />
-    </template>
-
-    <template v-slot:head(feesVsLiquidity)="data">
-      {{ data.label }}
-      <font-awesome-icon
-        v-b-popover.hover.top="toolTips.feesVsLiquidity"
-        icon="info-circle"
-      />
-    </template>
-
-    <template v-slot:cell(symbol)="data">
-      <pool-logos :pool="data.item" :cursor="false" />
-
-      <!-- <router-link :to="{ name: 'DetailsPool', params: { id: data.item.id } }">
-        <pool-logos :pool="data.item" :cursor="false" :version="true" />
-      </router-link>-->
-    </template>
-
-    <template v-slot:head(liquidityProtection)="data">
-      <img :src="require(`@/assets/media/icons/liquidity.svg`)" />
-    </template>
-
-    <template v-slot:cell(liquidityProtection)="data">
-      <img
-        v-if="data.value"
-        :src="require(`@/assets/media/icons/liquidity_active.svg`)"
-      />
-    </template>
-
-    <template v-slot:cell(actionButtons)="data">
-      <action-buttons :pool="data.item" />
-    </template>
-  </table-wrapper>
-
   <data-table
-    v-else
-    :fields="fields2"
+    :fields="fields"
     :items="items"
     v-model="paginatedItems"
     :filter="filter"
@@ -93,7 +27,6 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
-import TableWrapper from "@/components/common/TableWrapper.vue";
 import ActionButtons from "@/components/common/ActionButtons.vue";
 import PoolLogos from "@/components/common/PoolLogos.vue";
 import { ViewRelay } from "@/types/bancor";
@@ -103,7 +36,7 @@ import DataTable from "@/components/common/DataTable.vue";
 import { ViewTableFields } from "@/components/common/TableHeader.vue";
 
 @Component({
-  components: { DataTable, PoolLogos, ActionButtons, TableWrapper }
+  components: { DataTable, PoolLogos, ActionButtons }
 })
 export default class TablePools extends Vue {
   @Prop() items!: ViewRelay[];
@@ -131,7 +64,7 @@ export default class TablePools extends Vue {
     return tooltips;
   }
 
-  get fields2(): ViewTableFields[] {
+  get fields(): ViewTableFields[] {
     return [
       ...(this.isEth
         ? [
@@ -181,91 +114,8 @@ export default class TablePools extends Vue {
     ];
   }
 
-  get fields() {
-    return [
-      {
-        key: "liquidityProtection",
-        sortable: true
-      },
-      {
-        key: "symbol",
-        label: "Name",
-        sortable: true,
-        thStyle: { "min-width": "150px" }
-      },
-      {
-        key: "liqDepth",
-        label: "Liquidity",
-        thStyle: { "min-width": "150px" },
-        sortable: true,
-        formatter: (value: number) =>
-          new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD"
-          }).format(value)
-      },
-      {
-        key: "fee",
-        label: "Fee",
-        thStyle: { "min-width": "80px" },
-        sortable: true,
-        formatter: formatPercent
-      },
-      ...(this.isEth
-        ? [
-            {
-              key: "volume",
-              label: "Volume (24hr)",
-              sortable: true,
-              thStyle: { "min-width": "140px" },
-              formatter: (value: string) =>
-                value && new BigNumber(value).isGreaterThan(0)
-                  ? new Intl.NumberFormat("en-US", {
-                      style: "currency",
-                      currency: "USD"
-                    }).format(Number(value))
-                  : "N/A"
-            },
-            {
-              key: "feesGenerated",
-              label: "Fees (24hr)",
-              sortable: true,
-              thStyle: { "min-width": "100px" },
-              formatter: (value: string) =>
-                value && new BigNumber(value).isGreaterThan(0)
-                  ? new Intl.NumberFormat("en-US", {
-                      style: "currency",
-                      currency: "USD"
-                    }).format(Number(value))
-                  : "N/A"
-            },
-            {
-              key: "feesVsLiquidity",
-              label: "1y Fees / Liquidity",
-              sortable: true,
-              thStyle: { "min-width": "120px" },
-              formatter: (value: string) =>
-                value && new BigNumber(value).isGreaterThan(0)
-                  ? formatPercent(value)
-                  : "N/A"
-            }
-          ]
-        : []),
-      {
-        key: "actionButtons",
-        label: "Action",
-        thStyle: { width: "310px", "min-width": "310px" }
-      }
-    ];
-  }
-
   get isEth() {
     return this.$route.params.service == "eth";
-  }
-
-  doFilter(row: ViewRelay, filter: string) {
-    const symbols = row.reserves.map(reserve => reserve.symbol.toLowerCase());
-    return symbols.some(symbol => symbol.includes(filter.toLowerCase()));
   }
 }
 </script>
