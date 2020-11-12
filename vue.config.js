a562b2e8edda99aba4d3b75ef237ceea8e5c7224
Will Park <korea651@hanmail.net>
2020-11-12 11:24:17 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
+// const webpack = require('webpack')
+
 module.exports = {
   chainWebpack: (config) => {
     config.resolve.symlinks(false)
   },
+  // configureWebpack: {
+  //   plugins: [
+  //     new webpack.IgnorePlugin({
+  //       resourceRegExp: /^\.\/locale$/,
+  //       contextRegExp: /moment$/
+  //     })
+  //   ]
+  // },
   pluginOptions: {
     i18n: {
       locale: "en",
