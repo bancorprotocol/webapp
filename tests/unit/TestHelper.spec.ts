29323948a0866c527ed0f4faa816178b79d24b66
Jan Langheimer <langheimer@gmail.com>
2020-11-12 10:24:40 +0100

    remove skip test

--- tests/unit/TestHelper.spec.ts
+++ tests/unit/TestHelper.spec.ts
@@ -1,36 +1,6 @@
 import { calculatePositionFees } from "@/api/pureHelpers";
 
 describe("can calculate position fees", () => {
-  test.skip("test one", async () => {
-    const {
-      originalPoolTokenAmount,
-      currentPoolTokenSupply,
-      depositedAmount,
-      depositedReserveCurrentBalance,
-      opposingDepositedReserveCurrentBalance,
-      reserveRate
-    } = {
-      originalPoolTokenAmount: "1370909146698872748",
-      currentPoolTokenSupply: "11338379267323634752139485",
-      depositedAmount: "3874264344838181",
-      depositedReserveCurrentBalance: "10932903826342227802656",
-      opposingDepositedReserveCurrentBalance: "6847120160277936759163837",
-      reserveRate: "290.61911993101308091657"
-    };
-
-    // current result is -552144045218744
-
-    const res = calculatePositionFees(
-      originalPoolTokenAmount,
-      currentPoolTokenSupply,
-      depositedAmount,
-      depositedReserveCurrentBalance,
-      opposingDepositedReserveCurrentBalance,
-      reserveRate
-    );
-    expect(res).toBe("?");
-  });
-
   test("test two", async () => {
     const {
       originalPoolTokenAmount,
