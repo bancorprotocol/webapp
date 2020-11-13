f95381449d61d62d1e420bb1c4d70af29e9be341
Will Park <korea651@hanmail.net>
2020-11-13 13:38:55 +0900

    Adapt lightweight for lodash

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,26 @@ const webpack = require('webpack')
 module.exports = {
   chainWebpack: (config) => {
     config.resolve.symlinks(false)
+
+    config.module
+      .rule('js')
+      .use('babel-loader')
+      .loader('babel-loader')
+      .tap((options = {}) => {
+        options.plugins = [...(options.plugins || []), "lodash"]
+        return options
+      })
+
+    config
+      .plugin('lodash-webpack')
+      .use(require('lodash-webpack-plugin'), [])
   },
   configureWebpack: {
     plugins: [
       new webpack.IgnorePlugin({
         resourceRegExp: /^\.\/locale$/,
         contextRegExp: /moment$/
-      })
+      }),
     ]
   },
   pluginOptions: {
