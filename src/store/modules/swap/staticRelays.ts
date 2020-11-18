1d1c33b54021a938d7717c746284cfe458862f08
John Williamson <johnjwilliamson@live.com>
2020-11-18 15:07:17 +1000

    remove comments

--- src/store/modules/swap/staticRelays.ts
+++ src/store/modules/swap/staticRelays.ts
@@ -409,19 +409,13 @@ export const previousPoolFees: PreviousPoolFee[] = [
 ];
 
 export const highCapPools = [
-  // ETH/BNT
   "0xb1CD6e4153B2a390Cf00A6556b0fC1458C4A5533",
-  // WBTC/BNT
   "0xFEE7EeaA0c2f3F7C7e6301751a8dE55cE4D059Ec",
-  // USDT/BNT
   "0x5365B5BC56493F08A38E5Eb08E36cBbe6fcC8306",
-  // USDC/BNT
   "0x874d8dE5b26c9D9f6aA8d7bab283F9A9c6f777f4",
-  // DAI/BNT
   "0xE5Df055773Bf9710053923599504831c7DBdD697",
-  // LINK/BNT
   "0x04D0231162b4784b706908c787CE32bD075db9b7"
-]
+];
 
 export const findPreviousPoolFee = (
   previousPoolFees: PreviousPoolFee[],
