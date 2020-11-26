0e21f3adc8fdc9c78e0edde0ad7ee1751a975161
Jan Langheimer <langheimer@gmail.com>
2020-11-26 11:17:10 +0100

    move expandToken function to pureHelper file

--- src/api/eth/helpers.ts
+++ src/api/eth/helpers.ts
@@ -2,14 +2,6 @@ import BigNumber from "bignumber.js";
 import { isAddress } from "web3-utils";
 import { web3 } from "@/api/web3";
 
-export const expandToken = (amount: string | number, precision: number) => {
-  const trimmed = new BigNumber(amount).toFixed(precision, 1);
-  const inWei = new BigNumber(trimmed)
-    .times(new BigNumber(10).pow(precision))
-    .toFixed(0);
-  return inWei;
-};
-
 const addZeros = (numberOfZeros: number, noLeadingZeros: string) => {
   const zeros = [...Array(numberOfZeros)].map(() => "0");
   const res = ["0", "x", ...zeros, ...noLeadingZeros.split("").slice(2)].join(
--- src/api/pureHelpers.ts
+++ src/api/pureHelpers.ts
@@ -59,3 +59,11 @@ export const miningTknReward = (
       .dividedBy(protectedTkn)
   ).toNumber();
 };
+
+export const expandToken = (amount: string | number, precision: number) => {
+  const trimmed = new BigNumber(amount).toFixed(precision, 1);
+  const inWei = new BigNumber(trimmed)
+    .times(new BigNumber(10).pow(precision))
+    .toFixed(0);
+  return inWei;
+};
