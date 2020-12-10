666348396d9425837aa791dd196c5b1022183638
Jan Langheimer <langheimer@gmail.com>
2020-12-10 10:25:55 +0100

    add placeholder components

---
+++ src/components/rewards/WithdrawRewards.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    withdraw placeholder
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import BaseComponent from "@/components/BaseComponent.vue";
+
+@Component
+export default class WithdrawRewards extends BaseComponent {}
+</script>
+
+<style lang="scss" scoped></style>
