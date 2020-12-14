7c2831b4588be5a578aa757a6cec93c497f51248
Will Park <korea651@hanmail.net>
2020-12-14 19:41:49 +0900

    cleanup comments

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,6 @@ module.exports = {
   automock: false,
   setupFiles: ["./setupJest.js"],
 
-  // preset: "@vue/cli-plugin-unit-jest",
-  // moduleFileExtensions: ["js", "ts", "json", "vue", "node"],
-  // transform: {
-  //   '^.+\\.vue$': 'vue-jest',
-  //   '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-  //     'jest-transform-stub',
-  //   "^.+\\.(ts|tsx)$": "ts-jest",
-  //   "^.+\\.js$": "babel-jest"
-  // },
-  // transformIgnorePatterns: ['node_modules']
-
   moduleNameMapper: {
     "/api/helper(.*)": "<rootDir>/tests/unit/__mocks__/helpersMock.ts"
   },
