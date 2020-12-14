d7f23fe5b5406e74ee3be96cf563431588ce6483
Jan Langheimer <langheimer@gmail.com>
2020-12-14 11:25:49 +0100

    add rewards summry

--- src/components/rewards/RewardsSummary.vue
+++ src/components/rewards/RewardsSummary.vue
@@ -1,30 +1,60 @@
 <template>
-  <ContentBlock :no-header="true">
+  <ContentBlock
+    v-if="true"
+    :no-header="true"
+    :shadow-light="true"
+    :px0="true"
+    class="pt-3 px-3 mb-3"
+    :class="darkMode ? 'text-dark' : 'text-light'"
+  >
     <div class="d-flex justify-content-between align-items-center d-xl-none">
-      <span>{{ title }}</span>
-      <b-btn
-        variant="primary"
-        :to="{ name: 'AddProtectionHome' }"
-        style="width: 132px"
-      >
-        ReStake
-      </b-btn>
+      <span class="font-size-16 font-w500">{{ title }}</span>
+      <div>
+        <b-btn variant="outline-gray" style="width: 132px" class="mr-3">
+          Withdraw
+        </b-btn>
+        <b-btn
+          variant="primary"
+          :to="{ name: 'AddProtectionHome' }"
+          style="width: 132px"
+        >
+          ReStake
+        </b-btn>
+      </div>
     </div>
     <b-row>
       <b-col md="6" lg="3" xl="2" class="d-none d-xl-flex align-items-center">
-        <div class="font-size-16 font-w600">{{ title }}</div>
+        <div class="font-size-16 font-w500">{{ title }}</div>
       </b-col>
       <b-col
         v-for="(item, index) in summarizedRewards"
-        :key="item.la"
+        :key="item.id"
         cols="6"
-        lg="3"
+        lg="4"
         xl="2"
         class="d-flex flex-column align-items-center mt-3 mt-xl-0"
         :class="getItemStyleClass(index)"
       >
-        <div class="font-size-14 font-w600 text-primary">{{ item.value }}</div>
-        <div class="text-uppercase font-size-10 font-w500">{{ item.key }}</div>
+        <div class="font-size-14 font-w600">
+          {{ prettifyNumber(item.bnt) }}
+        </div>
+        <div class="font-size-12 font-w500 text-primary">
+          (~{{ prettifyNumber(item.usd, true) }})
+        </div>
+        <div
+          class="text-uppercase font-size-10 font-w500"
+          :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
+        >
+          {{ item.label }}
+        </div>
+      </b-col>
+      <b-col
+        md="6"
+        lg="3"
+        xl="2"
+        class="d-none d-xl-flex align-items-center justify-content-end"
+      >
+        <b-btn variant="outline-gray" class="btn-block">Withdraw</b-btn>
       </b-col>
       <b-col
         md="6"
@@ -34,15 +64,15 @@
       >
         <b-btn
           variant="primary"
-          style="width: 132px"
+          class="btn-block"
           :to="{ name: 'AddProtectionHome' }"
-          >Stake</b-btn
+          >ReStake</b-btn
         >
       </b-col>
     </b-row>
   </ContentBlock>
   <div
-    v-if="false"
+    v-else
     :id="darkMode ? 'protected-summary-dark' : 'protected-summary'"
     class="rounded p-3 mb-3"
     :class="darkMode ? 'text-dark' : 'text-light'"
@@ -92,18 +122,14 @@
 <script lang="ts">
 import { Component, Prop } from "vue-property-decorator";
 import { ViewProtectedLiquidity } from "@/types/bancor";
-import { stringifyPercentage } from "@/api/helpers";
+import { stringifyPercentage, prettifyNumber } from "@/api/helpers";
 import BaseComponent from "@/components/BaseComponent.vue";
 import ContentBlock from "@/components/common/ContentBlock.vue";
 interface ViewRewardsSummaryItem {
+  id: number;
   label: string;
   bnt: number;
   usd: number;
-}
-interface ViewRewardsSummary {
-  claimable: ViewRewardsSummaryItem;
-  future: ViewRewardsSummaryItem;
-  total: ViewRewardsSummaryItem;
 }
 
 @Component({
@@ -112,33 +138,37 @@ interface ViewRewardsSummary {
 export default class RewardsSummary extends BaseComponent {
   @Prop({ default: [] }) positions!: ViewProtectedLiquidity[];
   stringifyPercentage = stringifyPercentage;
+  prettifyNumber = prettifyNumber;
 
-  title = "Rewards";
+  title = "BNT Rewards";
 
-  get summarizedRewards(): ViewRewardsSummary {
-    return {
-      claimable: {
+  get summarizedRewards(): ViewRewardsSummaryItem[] {
+    return [
+      {
+        id: 1,
         label: "Claimable Rewards",
-        bnt: 1.123123,
+        bnt: 1231231231.123123123123,
         usd: 99.12
       },
-      future: {
+      {
+        id: 2,
         label: "Future Rewards",
         bnt: 1.123123,
         usd: 99.12
       },
-      future: {
+      {
+        id: 3,
         label: "Total Reward to date",
         bnt: 1.123123,
         usd: 99.12
       }
-    };
+    ];
   }
 
   getItemStyleClass(index: number) {
     const pos = index + 1;
     if (pos === 1) return "text-center";
-    else if (pos < this.summarizedPositions.length) return "text-center";
+    else if (pos < this.summarizedRewards.length) return "text-center";
     else return "text-center";
   }
 }
