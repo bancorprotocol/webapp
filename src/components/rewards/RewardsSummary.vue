b55a4daf4b0c211af639f27274face3bf9751e05
Jan Langheimer <langheimer@gmail.com>
2020-12-14 12:21:26 +0100

    add restake modal pool select

--- src/components/rewards/RewardsSummary.vue
+++ src/components/rewards/RewardsSummary.vue
@@ -1,75 +1,82 @@
 <template>
-  <ContentBlock
-    :no-header="true"
-    :shadow-light="true"
-    :px0="true"
-    class="pt-3 px-3 mb-3"
-    :class="darkMode ? 'text-dark' : 'text-light'"
-  >
-    <div class="d-flex justify-content-between align-items-center d-xl-none">
-      <span class="font-size-16 font-w500">{{ title }}</span>
-      <div>
-        <b-btn variant="outline-gray" style="width: 132px" class="mr-3">
-          Withdraw
-        </b-btn>
-        <b-btn
-          variant="primary"
-          :to="{ name: 'AddProtectionHome' }"
-          style="width: 132px"
-        >
-          ReStake
-        </b-btn>
+  <div>
+    <ContentBlock
+      :no-header="true"
+      :shadow-light="true"
+      :px0="true"
+      class="pt-3 px-3 mb-3"
+      :class="darkMode ? 'text-dark' : 'text-light'"
+    >
+      <div class="d-flex justify-content-between align-items-center d-xl-none">
+        <span class="font-size-16 font-w500">{{ title }}</span>
+        <div>
+          <b-btn
+            variant="outline-gray"
+            :to="{ name: 'RewardsWithdraw' }"
+            style="width: 132px"
+            class="mr-3"
+          >
+            Withdraw
+          </b-btn>
+          <b-btn variant="primary" @click="openModal" style="width: 132px">
+            ReStake
+          </b-btn>
+        </div>
       </div>
-    </div>
-    <b-row>
-      <b-col md="6" lg="3" xl="2" class="d-none d-xl-flex align-items-center">
-        <div class="font-size-16 font-w500">{{ title }}</div>
-      </b-col>
-      <b-col
-        v-for="(item, index) in summarizedRewards"
-        :key="item.id"
-        cols="6"
-        lg="4"
-        xl="2"
-        class="d-flex flex-column align-items-center mt-3 mt-xl-0"
-        :class="getItemStyleClass(index)"
-      >
-        <div class="font-size-14 font-w600">
-          {{ prettifyNumber(item.bnt) }} BNT
-        </div>
-        <div class="font-size-12 font-w500 text-primary">
-          (~{{ prettifyNumber(item.usd, true) }})
-        </div>
-        <div
-          class="text-uppercase font-size-10 font-w500"
-          :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
+      <b-row>
+        <b-col md="6" lg="3" xl="2" class="d-none d-xl-flex align-items-center">
+          <div class="font-size-16 font-w500">{{ title }}</div>
+        </b-col>
+        <b-col
+          v-for="(item, index) in summarizedRewards"
+          :key="item.id"
+          cols="6"
+          lg="4"
+          xl="2"
+          class="d-flex flex-column align-items-center mt-3 mt-xl-0"
+          :class="getItemStyleClass(index)"
         >
-          {{ item.label }}
-        </div>
-      </b-col>
-      <b-col
-        md="6"
-        lg="3"
-        xl="2"
-        class="d-none d-xl-flex align-items-center justify-content-end"
-      >
-        <b-btn variant="outline-gray" class="btn-block">Withdraw</b-btn>
-      </b-col>
-      <b-col
-        md="6"
-        lg="3"
-        xl="2"
-        class="d-none d-xl-flex align-items-center justify-content-end"
-      >
-        <b-btn
-          variant="primary"
-          class="btn-block"
-          :to="{ name: 'AddProtectionHome' }"
-          >ReStake</b-btn
+          <div class="font-size-14 font-w600">
+            {{ prettifyNumber(item.bnt) }} BNT
+          </div>
+          <div class="font-size-12 font-w500 text-primary">
+            (~{{ prettifyNumber(item.usd, true) }})
+          </div>
+          <div
+            class="text-uppercase font-size-10 font-w500"
+            :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
+          >
+            {{ item.label }}
+          </div>
+        </b-col>
+        <b-col
+          md="6"
+          lg="3"
+          xl="2"
+          class="d-none d-xl-flex align-items-center justify-content-end"
         >
-      </b-col>
-    </b-row>
-  </ContentBlock>
+          <b-btn
+            variant="outline-gray"
+            class="btn-block"
+            :to="{ name: 'RewardsWithdraw' }"
+            >Withdraw</b-btn
+          >
+        </b-col>
+        <b-col
+          md="6"
+          lg="3"
+          xl="2"
+          class="d-none d-xl-flex align-items-center justify-content-end"
+        >
+          <b-btn variant="primary" class="btn-block" @click="openModal">
+            ReStake
+          </b-btn>
+        </b-col>
+      </b-row>
+    </ContentBlock>
+
+    <modal-pool-select @select="selectPool" v-model="modal" :pools="pools" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -78,6 +85,8 @@ import { ViewProtectedLiquidity } from "@/types/bancor";
 import { stringifyPercentage, prettifyNumber } from "@/api/helpers";
 import BaseComponent from "@/components/BaseComponent.vue";
 import ContentBlock from "@/components/common/ContentBlock.vue";
+import ModalPoolSelect from "@/components/modals/ModalSelects/ModalPoolSelect.vue";
+import { vxm } from "@/store";
 interface ViewRewardsSummaryItem {
   id: number;
   label: string;
@@ -86,7 +95,7 @@ interface ViewRewardsSummaryItem {
 }
 
 @Component({
-  components: { ContentBlock }
+  components: { ModalPoolSelect, ContentBlock }
 })
 export default class RewardsSummary extends BaseComponent {
   @Prop({ default: [] }) positions!: ViewProtectedLiquidity[];
@@ -94,6 +103,11 @@ export default class RewardsSummary extends BaseComponent {
   prettifyNumber = prettifyNumber;
 
   title = "Rewards";
+  modal = false;
+
+  get pools() {
+    return vxm.bancor.relays.filter(pool => pool.liquidityProtection);
+  }
 
   get summarizedRewards(): ViewRewardsSummaryItem[] {
     return [
@@ -116,6 +130,18 @@ export default class RewardsSummary extends BaseComponent {
         usd: 99.12
       }
     ];
+  }
+
+  openModal() {
+    this.modal = true;
+  }
+
+  selectPool(id: string) {
+    this.modal = false;
+    this.$router.push({
+      name: "RewardsRestake",
+      params: { id }
+    });
   }
 
   getItemStyleClass(index: number) {
