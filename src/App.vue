2e2e1164c1c41c0720172f53ed22bcc2d87fc48a
John Williamson <johnjwilliamson@live.com>
2020-12-17 18:01:30 +1000

    remove full stop

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@
     <div
       class="d-block mb-0 py-2 bg-primary text-white text-center font-size-12 font-w600"
     >
-      This interface is in beta. Use it at your own risk..
+      This interface is in beta. Use it at your own risk.
     </div>
     <div name="MainLayout" class="main-layout">
       <side-bar />
