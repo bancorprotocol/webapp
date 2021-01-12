93013b211d65bee6f91bf611fc5ccf713fef9aa2
Will Park <korea651@hanmail.net>
2021-01-12 15:04:12 +0900

    mocking helpers

--- tests/unit/__mocks__/helpersMock.ts
+++ tests/unit/__mocks__/helpersMock.ts
@@ -1,3 +1,12 @@
 export const prettifyNumber = (): string => {
   return "";
 };
+
+export const compareString = (stringOne: string, stringTwo: string) => {
+  const strings = [stringOne, stringTwo];
+  if (!strings.every(str => typeof str == "string"))
+    throw new Error(
+      `String one: ${stringOne} String two: ${stringTwo} one of them are falsy or not a string`
+    );
+  return stringOne.toLowerCase() == stringTwo.toLowerCase();
+};
