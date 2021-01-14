f5fcb0d34d7093d03f931f88f5463bbde5a61ac2
Will Park <korea651@hanmail.net>
2021-01-14 16:07:23 +0900

    Pass the test

--- tests/unit/momentToDayjs.spec.ts
+++ tests/unit/momentToDayjs.spec.ts
@@ -1,11 +1,19 @@
 //run `yarn test:unit -i momentToDayjs.spec.ts`
 
-/*
-import moment from "moment";
+// import moment from "moment";
 import dayjs from "@/utils/dayjs"
 
 describe("check moment and dayjs", () => {  
+  test("dayjs", () => {
+    const unixTime = 1610107544
+    const oneDayD = dayjs.duration(1, "day")
+    const yesterDayD = dayjs().subtract(oneDayD)
 
+    expect(dayjs.unix(unixTime).isBefore(dayjs())).toBeTruthy()
+    expect(dayjs().isSameOrBefore(yesterDayD)).toBeFalsy()
+  })
+
+  /*
   test("durationToSeconds", () => {
     // console.log(moment.duration(3, "days").asSeconds())
     // console.log(dayjs.duration(3, "days").asSeconds())
@@ -141,7 +149,7 @@ describe("check moment and dayjs", () => {
       clearInterval()
     }, interval);
   })
+  */
 
 })
 
-*/
