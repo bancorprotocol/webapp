2c2dead32b84bb7dd8e90d19bdf8649c9420f781
Will Park <korea651@hanmail.net>
2021-01-25 15:04:47 +0900

    Start support ko

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,14 @@ import Vue from "vue";
 import VueI18n from "vue-i18n";
 import { en } from "./en";
 import { he } from "./he";
+import { ko } from "./ko";
 
 Vue.use(VueI18n);
 
 const messages = {
   en,
-  he
+  he,
+  ko
 };
 
 export const i18n = new VueI18n({
@@ -22,6 +24,8 @@ export const getLanguageByLocale = (locale: string): string => {
       return "English";
     case "he":
       return "עברית";
+    case "ko":
+      return "한국어";
     default:
       return "English";
   }
