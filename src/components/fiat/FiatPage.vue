e285d61a63d0d7010d21bdbcdb61d1440242506b
Will Park <korea651@hanmail.net>
2021-01-26 16:07:59 +0900

    Add fiatpage

--- src/components/fiat/FiatPage.vue
+++ src/components/fiat/FiatPage.vue
@@ -1,10 +1,36 @@
 <template>
   <b-row>
-    <b-col class="col-12" sm="3" lg="2">
-      Fiat
+    <b-col cols="12">
+      <div>
+        <span
+          class="font-size-20 font-w600"
+          :class="darkMode ? 'text-dark' : 'text-light'"
+         >
+          Crypto/Fiat gateway providers
+        </span>
+      </div>
+
+      <p
+        class="font-size-14 font-w400 my-3"
+        :class="darkMode ? 'text-dark' : 'text-light'"
+      >
+        Here is a list of popular crypto/fiat gateways that can be used to buy crypto with credit card,  
+        wire transfer, and more. Some also offer crypto to fiat off-ramp solutions.
+      </p>
     </b-col>
-    <b-col class="col-12" sm="9" lg="10">
-      
+
+    <b-col cols="12">
+      <alert-block
+        class="my-2"
+      >
+        Disclaimer: The services are provided by the different service providers as is. 
+        Bancor is not associated, responsible or liable to the performance of the services. 
+        Any claims and questions, should be addressed with the selected partner directly.
+      </alert-block>  
+    </b-col>
+
+    <b-col cols="12">
+      <gateway-providers />
     </b-col>
   </b-row>
 </template>
@@ -13,9 +39,13 @@
 import { Component } from "vue-property-decorator";
 import { vxm } from "@/store";
 import BaseComponent from "@/components/BaseComponent.vue";
+import AlertBlock from "@/components/common/AlertBlock.vue";
+import GatewayProviders from "@/components/fiat/GatewayProviders"
 
 @Component({
   components: {
+    AlertBlock,
+    GatewayProviders
   }
 })
 export default class FiatPage extends BaseComponent {
---
+++ src/components/fiat/GatewayProviders.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-row>
+    <b-col lg="4">
+      Moonpay
+    </b-col>
+    <b-col lg="4">
+
+    </b-col>
+    <b-col lg="4">
+
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+
+</script>
+<style lang="scss"></style>
