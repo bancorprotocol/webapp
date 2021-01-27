48d81aac3043499bf5db2d974f236419cb7799a0
Will Park <korea651@hanmail.net>
2021-01-27 16:20:09 +0900

    styling ongoing

--- src/components/fiat/GatewayProviders.vue
+++ src/components/fiat/GatewayProviders.vue
@@ -1,61 +1,111 @@
 <template>
   <b-row>
-    <b-col lg="4">
+    <b-col lg="3">
       <img
         class="provider-icon"
         :src="require(`@/assets/media/logos/black-mp-logo.png`)"
       />
     </b-col>
-    <b-col lg="4">
-      <div class="font-size-12 font-w400">
+    <b-col lg="5">
+      <div class="name-provider font-size-16 font-w500 mb-2">
         MoonPay
       </div>
-      <div class="font-size-11 font-w300">
-        AVAILABLE OPERATIONS: 
-        <span>fiat in</span>
-        <span>fiat out</span>
-      </div>
-      <div class="font-size-11 font-w400">
+      <b-row class="font-size-12 font-w600 ml-0 my-2" align-v="center">
+        <div class="txt-available mr-1">availble operations: </div>
+        <div class="fiat fiat__in mx-1">fiat in</div>
+        <div class="fiat fiat__out mx-1">fiat out</div>
+      </b-row>
+      <div class="font-size-14 font-w400">
         MoonPay is a financial technology company that builds payments infrastructure for crypto. 
         Our on-and-off-ramp suite of products provides a seamless experience 
         for converting between fiat currencies and cryptocurrencies using all major payment methods.
       </div>
-      <b-btn
-        variant="primary"
-        @click="click"
-        class="font-size-14 font-w500 px-4"
-        >Buy</b-btn>
-      <b-btn
-        variant="primary"
-        @click="click"
-        class="font-size-14 font-w500 px-4"
-        >Sell</b-btn>
+      <div class="mt-3">
+        <b-btn
+          variant="primary"
+          @click="buy"
+          class="font-size-14 font-w500 px-5 py-2 mr-2"
+          >Buy</b-btn>
+        <b-btn
+          variant="primary"
+          @click="sell"
+          class="font-size-14 font-w500 px-5 py-2 ml-2"
+          >Sell</b-btn>
+      </div>
     </b-col>
     <b-col lg="4">
-      <div class="font-size-11 font-w300">
+      <div class="check-header font-size-12 font-w600">
         DEPOSIT METHODS:
       </div>
       <b-row>
         <b-col>
-          <div>Wire Transfer</div>
-          <div>Credit Card</div>
-          <div>Debit Card</div>
-          <div>Apple Pay</div>
+          <div><font-awesome-icon icon="check-circle" /> Wire Transfer</div>
+          <div><font-awesome-icon icon="check-circle" /> Credit Card</div>
+          <div><font-awesome-icon icon="check-circle" /> Debit Card</div>
+          <div><font-awesome-icon icon="check-circle" /> Apple Pay</div>
         </b-col>
         <b-col>
-          <div>Google Pay</div>
-          <div>Samsung Pay</div>
+          <div><font-awesome-icon icon="check-circle" /> Google Pay</div>
+          <div><font-awesome-icon icon="check-circle" /> Samsung Pay</div>
         </b-col>
       </b-row>
-      <div class="font-size-11 font-w300">
+      <div class="check-header font-size-12 font-w600">
         JURISDICTIONS:
-        <div>Global</div>
       </div>
+      <b-row>
+        <b-col>
+          <div><font-awesome-icon icon="check-circle" /> Global</div>
+        </b-col>
+      </b-row>
     </b-col>
   </b-row>
 </template>
 
 <script lang="ts">
+import BaseComponent from "@/components/BaseComponent.vue";
 
+export default class GatewayProviders extends BaseComponent {
+
+  buy() {
+    window.open("https://buy.moonpay.com/?apiKey=pk_live_qiJRayRj9iKRUWCfaWX5Vo6Sn0rnNsj&currencyCode=ETH", "_blank");
+  }
+
+  sell() {
+    window.open("https://sell.moonpay.com/?apiKey=pk_live_qiJRayRj9iKRUWCfaWX5Vo6Sn0rnNsj", "_blank");
+  }
+}
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+
+.txt-available {
+  text-transform: uppercase;
+  color: #6E7E94;
+  line-height: 15px;
+}
+
+.fiat {
+  width: 70px;
+  height: 24px;
+  border-radius: 8px;
+  background: #D5FFFF;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  text-align: center;
+
+  &__in {
+    background: #D5FFFF;
+  }
+
+  &__out {
+    background: #E9F2FD;
+  }
+}
+
+.check-header {
+  text-transform: uppercase;
+  line-height: 15px;
+  color: #6E7E94;
+}
+
+</style>
