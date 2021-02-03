a9ab3e1a753a1ffd3fd1c3731309725b9771bec8
Jan Langheimer <langheimer@gmail.com>
2021-02-03 14:11:32 +0100

    temporary disable price deviation

--- src/store/modules/swap/ethBancor.ts
+++ src/store/modules/swap/ethBancor.ts
@@ -2452,6 +2452,8 @@ export class EthBancorModule
       selectedTokenAddress
     ]).map(x => x.contract);
 
+    console.log("janjan selectedTokenAddress", selectedTokenAddress);
+
     const [
       recentAverageRateResult,
       averageRateMaxDeviationResult,
@@ -2473,6 +2475,12 @@ export class EthBancorModule
       new BigNumber(primaryReserveBalanceResult),
       new BigNumber(secondaryReserveBalanceResult),
       new BigNumber(averageRateMaxDeviationResult)
+    );
+
+    console.log("janjan recentAverageRateResult", recentAverageRateResult);
+    console.log(
+      "janjan averageRateMaxDeviationResult",
+      averageRateMaxDeviationResult
     );
 
     return priceDeviationTooHigh;
