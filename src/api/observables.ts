e96c70eeb5424da70895821ae950595fcedabd28
John Williamson <johnjwilliamson@live.com>
2021-02-03 15:04:10 +1000

    remove timing

--- src/api/observables.ts
+++ src/api/observables.ts
@@ -358,8 +358,6 @@ const verifiedPositions$ = combineLatest([
   )
 );
 
-const timeStart = Date.now();
-
 combineLatest([
   verifiedPositions$,
   liquidityProtectionStore$,
@@ -376,9 +374,6 @@ combineLatest([
   ]) => {
     const supportedAnchors = apiData.pools.map(pool => pool.pool_dlt_id);
 
-    const timeNow = Date.now();
-    const difference = timeNow - timeStart;
-    console.log("difference", difference, difference / 1000);
     vxm.ethBancor
       .buildFullPositions({
         rawPositions,
