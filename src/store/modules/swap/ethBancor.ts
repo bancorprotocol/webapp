81046c93f06854b5125e8326c05e50bb677ac077
John Williamson <johnjwilliamson@live.com>
2021-02-03 15:03:19 +1000

    mark difference

--- src/store/modules/swap/ethBancor.ts
+++ src/store/modules/swap/ethBancor.ts
@@ -1556,6 +1556,11 @@ export class EthBancorModule
   protectedPositionsArr: ProtectedLiquidityCalculated[] = [];
 
   @mutation setProtectedPositions(positions: ProtectedLiquidityCalculated[]) {
+    console.log(positions, "are the positions getting set!");
+
+    const timeEnd = Date.now();
+    const difference = timeEnd - timeStart;
+    console.log("difference", difference, difference / 1000);
     this.protectedPositionsArr = positions;
   }
 
