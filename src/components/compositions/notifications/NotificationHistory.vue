b81150b459f3d539566b5f86ac8258c5ca6fcfc9
Jan Langheimer <langheimer@gmail.com>
2021-02-18 16:16:24 +0100

    add test notifications

--- src/components/compositions/notifications/NotificationHistory.vue
+++ src/components/compositions/notifications/NotificationHistory.vue
@@ -1,22 +1,61 @@
 <template>
   <div>
+    <b-btn
+      @click="addNotification('Test Title', 'Description goes here', 'success')"
+      size="sm"
+      class="mr-2"
+    >
+      All
+    </b-btn>
     <div v-if="history.length">
       <div class="d-flex justify-content-between">
         <div>
-          <b-btn size="sm" class="mr-2">All</b-btn>
-          <b-btn size="sm" class="mr-2">pending</b-btn>
-          <b-btn size="sm" class="mr-2">success</b-btn>
-          <b-btn size="sm" class="mr-2">info</b-btn>
-          <b-btn size="sm">error</b-btn>
+          <b-btn @click="addNotification('Test Title')" size="sm" class="mr-2">
+            default
+          </b-btn>
+          <b-btn
+            @click="
+              addNotification('Test Title', 'Description goes here', 'pending')
+            "
+            size="sm"
+            class="mr-2"
+          >
+            pending
+          </b-btn>
+          <b-btn
+            @click="
+              addNotification('Test Title', 'Description goes here', 'success')
+            "
+            size="sm"
+            class="mr-2"
+          >
+            success
+          </b-btn>
+          <b-btn
+            @click="
+              addNotification('Test Title', 'Description goes here', 'warning')
+            "
+            size="sm"
+          >
+            warning
+          </b-btn>
+          <b-btn
+            @click="
+              addNotification('Test Title', 'Description goes here', 'error')
+            "
+            size="sm"
+          >
+            error
+          </b-btn>
         </div>
 
         <b-btn @click="clearAllNotifications()" size="sm">clear</b-btn>
       </div>
       <notification-details
         v-for="notification in history"
-        :key="notification.id"
+        :key="`history-${notification.id}`"
         :notification="notification"
-        class="mb-2"
+        class="my-2"
         style="width: 100%"
       />
     </div>
@@ -29,7 +68,8 @@ import { defineComponent } from "@vue/composition-api";
 import {
   clearAllNotifications,
   history,
-  pendingQueue
+  pendingQueue,
+  addNotification
 } from "@/components/compositions/notifications/index";
 import NotificationDetails from "@/components/compositions/notifications/NotificationDetails.vue";
 
@@ -40,7 +80,8 @@ export default defineComponent({
     return {
       history,
       pendingQueue,
-      clearAllNotifications
+      clearAllNotifications,
+      addNotification
     };
   }
 });
