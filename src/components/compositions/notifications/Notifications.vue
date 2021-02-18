793530689aea2b90256efb08b4ab33e0ebab807e
Jan Langheimer <langheimer@gmail.com>
2021-02-18 16:16:42 +0100

    refactor

--- src/components/compositions/notifications/Notifications.vue
+++ src/components/compositions/notifications/Notifications.vue
@@ -1,24 +1,28 @@
 <template>
-  <b-dropdown
-    id="dropdown-activity"
-    right
-    :variant="darkMode ? 'outline-dark' : 'outline-light'"
-    size="sm"
-    toggle-class="block-rounded"
-    :menu-class="darkMode ? 'bg-block-dark shadow' : 'bg-block-light shadow'"
-    no-caret
-  >
-    <template #button-content>
-      <font-awesome-icon icon="cog" fixed-width />
-    </template>
-
-    <b-dropdown-text
-      style="width: 500px"
-      :variant="darkMode ? 'dark' : 'light'"
+  <div>
+    <b-dropdown
+      id="dropdown-activity"
+      right
+      :variant="darkMode ? 'outline-dark' : 'outline-light'"
+      size="sm"
+      toggle-class="block-rounded"
+      :menu-class="darkMode ? 'bg-block-dark shadow' : 'bg-block-light shadow'"
+      no-caret
     >
-      <NotificationHistory />
-    </b-dropdown-text>
-  </b-dropdown>
+      <template #button-content>
+        <font-awesome-icon icon="cog" fixed-width />
+      </template>
+
+      <b-dropdown-text
+        style="width: 500px"
+        :variant="darkMode ? 'dark' : 'light'"
+      >
+        <NotificationHistory />
+      </b-dropdown-text>
+    </b-dropdown>
+
+    <NotificationAlerts />
+  </div>
 </template>
 
 <script lang="ts">
@@ -26,20 +30,23 @@ import { defineComponent } from "@vue/composition-api";
 import {
   alertQueue,
   addNotification,
-  ENotificationStatus
+  ENotificationStatus,
+  loadLocalHistory
 } from "@/components/compositions/notifications/index";
 import NotificationHistory from "@/components/compositions/notifications/NotificationHistory.vue";
 import { vxm } from "@/store";
+import NotificationAlerts from "@/components/compositions/notifications/NotificationAlerts.vue";
 
 export default defineComponent({
   props: { position: { type: String, default: "right" } },
-  components: { NotificationHistory },
+  components: { NotificationHistory, NotificationAlerts },
   computed: {
     darkMode() {
       return vxm.general.darkMode;
     }
   },
   setup() {
+    loadLocalHistory();
     return {
       alertQueue,
       addNotification,
