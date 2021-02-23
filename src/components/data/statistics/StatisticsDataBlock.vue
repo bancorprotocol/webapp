db9551168433157395b88c4451592e4650dda97d
Ran Cohen <ran@bancor.network>
2021-02-23 14:56:25 +0200

    Added percentage and colors.

--- src/components/data/statistics/StatisticsDataBlock.vue
+++ src/components/data/statistics/StatisticsDataBlock.vue
@@ -8,16 +8,17 @@
     </span>
     <div>
       <span
-        class="font-size-14 font-w600"
+        class="font-size-20 font-w600"
         :class="darkMode ? 'text-dark' : 'text-light'"
       >
         {{ value }}
       </span>
       <span
-        v-if="typeof percentage !== 'undefined'"
-        :class="percentage >= 0 ? 'text-success' : 'text-danger'"
-        >{{ percentage }}%</span
-      >
+        v-if="percentage"
+        class="font-size-14"
+        :class="percentage >= 0 ? 'percentage-positive' : 'percentage-negative'"
+        >{{ `${percentage > 0 ? "+" : ""}${percentage}%` }}
+      </span>
     </div>
   </div>
 </template>
