42d8e847629172b85d645229c60bdc97d8966b1e
John Williamson <johnjwilliamson@live.com>
2021-03-02 16:25:18 +1000

    remove stale import

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -26,7 +26,7 @@
 
 <script lang="ts">
 import { i18n } from "@/i18n";
-import { Component, Watch } from "vue-property-decorator";
+import { Component } from "vue-property-decorator";
 import SideBarLeft from "@/components/layout/SideBarLeft.vue";
 import SideBarBottom from "@/components/layout/SideBarBottom.vue";
 import SideBarBottomMore from "@/components/layout/SideBarBottomMore.vue";
