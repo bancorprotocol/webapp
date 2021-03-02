d6cfba7dcbfce773816abdb9dd01d69a545d3817
Ran Cohen <ran@bancor.network>
2021-03-02 15:01:59 +0200

    Removed pool route

---
+++ src/components/modals/ModalSelects/ModalDurationSelect.vue
@@ -0,0 +1,137 @@
+<template>
+  <modal-base :title="title" v-model="show">
+    <b-row class="d-flex justify-content-center">
+      <b-col cols="12" class="text-center">
+        <font-awesome-icon icon="clock" class="text-primary" size="3x" />
+        <div
+          :class="darkMode ? 'text-dark' : 'text-light'"
+          class="font-size-20 font-w600"
+        >
+          {{ $t("custom_time") }}
+        </div>
+        <div
+          :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
+          class="font-size-12 font-w400"
+        >
+          {{ $t("set_up_time") }}
+        </div>
+      </b-col>
+      <b-dropdown
+        boundary="viewport"
+        :variant="darkMode ? 'outline-dark' : 'outline-light'"
+        toggle-class="block-rounded"
+        :menu-class="
+          darkMode ? 'bg-block-dark shadow' : 'bg-block-light shadow'
+        "
+        cols="6"
+        class="mr-2 mt-3"
+      >
+        <template #button-content>
+          {{ formatDuration(selectedHours) }}
+        </template>
+
+        <b-dropdown-item
+          v-for="item in 24"
+          :key="item"
+          @click="changeHours(item)"
+          :variant="darkMode ? 'dark' : 'light'"
+        >
+          <div class="d-flex justify-content-between">
+            {{ `${item} ${$t("hours")}` }}
+            <font-awesome-icon
+              v-if="selectedHours.hours() === item"
+              icon="check"
+              class="mr-2 menu-icon"
+            />
+          </div>
+        </b-dropdown-item>
+      </b-dropdown>
+      <b-dropdown
+        boundary="viewport"
+        :variant="darkMode ? 'outline-dark' : 'outline-light'"
+        toggle-class="block-rounded"
+        :menu-class="
+          darkMode ? 'bg-block-dark shadow' : 'bg-block-light shadow'
+        "
+        cols="6"
+        class="mt-3"
+      >
+        <template #button-content>
+          {{ formatDuration(selectedMinutes) }}
+        </template>
+
+        <b-dropdown-item
+          v-for="item in 60"
+          :key="item"
+          @click="changeMinutes(item)"
+          :variant="darkMode ? 'dark' : 'light'"
+        >
+          <div class="d-flex justify-content-between">
+            {{ `${item} ${$t("minutes")}` }}
+            <font-awesome-icon
+              v-if="selectedMinutes.minutes() === item"
+              icon="check"
+              class="mr-2 menu-icon"
+            />
+          </div>
+        </b-dropdown-item>
+      </b-dropdown>
+      <b-col cols="12" class="mt-3">
+        <main-button
+          :label="$t('confirm')"
+          :large="true"
+          :active="true"
+          @click="confirm()"
+        />
+      </b-col>
+    </b-row>
+  </modal-base>
+</template>
+<script lang="ts">
+import { Component, Emit, Prop, VModel } from "vue-property-decorator";
+import BaseComponent from "@/components/BaseComponent.vue";
+import ModalBase from "@/components/modals/ModalBase.vue";
+import MainButton from "@/components/common/Button.vue";
+import { formatDuration } from "@/api/helpers";
+import dayjs from "@/utils/dayjs";
+
+@Component({
+  components: { ModalBase, MainButton }
+})
+export default class ModalDurationSelect extends BaseComponent {
+  @VModel() show!: boolean;
+  @Prop({ type: String, default: "" }) title!: string;
+
+  selectedHours = dayjs.duration({ hours: 1 });
+  selectedMinutes = dayjs.duration({ minutes: 1 });
+
+  formatDuration(duration: plugin.Duration) {
+    return formatDuration(duration);
+  }
+
+  changeHours(hours: number) {
+    this.selectedHours = dayjs.duration({ hours });
+  }
+
+  changeMinutes(minutes: number) {
+    this.selectedMinutes = dayjs.duration({ minutes });
+  }
+
+  @Emit("confirm")
+  confirm() {
+    this.show = false;
+    return this.selectedHours.add({
+      minutes: this.selectedMinutes.asMinutes()
+    });
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.modal-dialog-scrollable .modal-content {
+  overflow: initial;
+}
+.modal-dialog-scrollable .modal-body {
+  position: static;
+}
+</style>
