87e2e0d35764c1fe75b6ecd28d9c4557e1765523
Jan Langheimer <langheimer@gmail.com>
2021-03-24 21:13:59 +0100

    add cancel limit order modal

---
+++ src/components/modals/ModalCancelOrder.vue
@@ -0,0 +1,97 @@
+<template>
+  <modal-base v-model="show" size="sm" @onHide="onHide">
+    <div class="text-center" :class="darkMode ? 'text-dark' : 'text-light'">
+      <div class="d-flex justify-content-center mb-3">
+        <div
+          class="d-flex justify-content-center align-items-center bg-danger rounded-circle"
+          style="width: 60px; height: 60px"
+        >
+          <font-awesome-icon icon="times" class="text-white" size="2x" />
+        </div>
+      </div>
+
+      <div class="font-size-20 font-w600 mb-3">Cancel Transaction</div>
+
+      <p
+        class="font-size-14 mb-4 text-center"
+        :class="darkMode ? 'text-muted-dark' : 'text-muted'"
+      >
+        {{ $t("modal.limit_order.sub_title") }}
+      </p>
+      <gray-border-block gray-bg="true">
+        <label-content-split
+          :label="$t('modal.limit_order.sell')"
+          :value="`${prettifyNumber(limitOrder.from.amount)} ${
+            limitOrder.from.symbol
+          }`"
+          class="mb-2"
+        />
+        <label-content-split
+          :label="$t('modal.limit_order.receive')"
+          :value="`${prettifyNumber(limitOrder.to.amount)} ${
+            limitOrder.to.symbol
+          }`"
+          class="mb-2"
+        />
+        <label-content-split
+          :label="$t('modal.limit_order.rate')"
+          :value="rate"
+        />
+      </gray-border-block>
+
+      <p
+        class="font-size-12 my-3 text-left pl-3"
+        :class="darkMode ? 'text-muted-dark' : 'text-muted'"
+      >
+        {{
+          $t("modal.limit_order.info_text", {
+            timer: "?????"
+          })
+        }}
+      </p>
+
+      <b-btn
+        @click="confirm"
+        class="mt-2 rounded py-2 btn-block"
+        variant="primary"
+      >
+        Confirm
+      </b-btn>
+    </div>
+  </modal-base>
+</template>
+<script lang="ts">
+import { Component, Emit, Prop, VModel } from "vue-property-decorator";
+import BaseComponent from "@/components/BaseComponent.vue";
+import ModalBase from "@/components/modals/ModalBase.vue";
+import MainButton from "@/components/common/Button.vue";
+import dayjs from "@/utils/dayjs";
+import GrayBorderBlock from "@/components/common/GrayBorderBlock.vue";
+import LabelContentSplit from "@/components/common/LabelContentSplit.vue";
+import { ViewLimitOrder } from "@/store/modules/swap/ethBancor";
+@Component({
+  components: { LabelContentSplit, GrayBorderBlock, ModalBase, MainButton }
+})
+export default class ModalCancelOrder extends BaseComponent {
+  @VModel() show!: boolean;
+  @Prop() limitOrder!: ViewLimitOrder;
+
+  get rate() {
+    const fromSymbol = this.limitOrder.from.symbol;
+    const toSymbol = this.limitOrder.to.symbol;
+    const rate = this.limitOrder.to.amount / this.limitOrder.from.amount;
+
+    return `1 ${fromSymbol} = ${this.prettifyNumber(rate)} ${toSymbol}`;
+  }
+
+  @Emit("confirm")
+  confirm() {
+    return;
+  }
+
+  @Emit("onHide")
+  onHide() {
+    return;
+  }
+}
+</script>
