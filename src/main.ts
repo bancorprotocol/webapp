8890070251e8fae1f6b2f3664fee64575365cf14
Ran Cohen <ran@bancor.network>
2021-04-28 11:36:24 +0300

    Removed gtag and cleared stale code

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,6 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { sync } from "vuex-router-sync";
 import { firebase } from "@firebase/app";
 import "@firebase/analytics";
-import VueGtag from "vue-gtag";
 
 const appVersion = JSON.parse(
   unescape(escape(JSON.stringify(require("../package.json"))))
@@ -59,14 +58,6 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-Vue.use(
-  VueGtag,
-  {
-    config: { id: "UA-174155472-1" }
-  },
-  router
-);
 
 Vue.use(BootstrapVue);
 
