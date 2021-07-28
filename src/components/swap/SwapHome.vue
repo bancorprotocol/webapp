8be2f8e731b84274ce13f8c5ff445a4e382d9e2a
John Williamson <johnjwilliamson@live.com>
2021-07-28 21:11:13 +1000

    add beta banner

--- src/components/swap/SwapHome.vue
+++ src/components/swap/SwapHome.vue
@@ -1,44 +1,53 @@
 <template>
-  <content-block :no-header="true" class="mb-3" :shadow="true">
-    <b-button-group class="d-flex w-100 text-center mt-2 mb-3 pt-3">
-      <b-button
-        id="market"
-        @click="limit = false"
-        :variant="
-          limit ? 'outline-primary' + (darkMode ? '-dark' : '') : 'primary'
-        "
-        >{{ $t("market") }}
-      </b-button>
-      <b-button
-        id="limit"
-        @click="limit = true"
-        :variant="
-          !limit ? 'outline-primary' + (darkMode ? '-dark' : '') : 'primary'
-        "
-        >{{ $t("limit") }}
-      </b-button>
-    </b-button-group>
-    <b-popover
-      target="market"
-      triggers="hover"
-      placement="left"
-      class="font-size-12 font-w400"
-      :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
-    >
-      {{ $t("exe_market_rate") }}
-    </b-popover>
-    <b-popover
-      target="limit"
-      triggers="hover"
-      placement="right"
-      class="font-size-12 font-w400"
-      :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
-    >
-      {{ $t("trade_at_pre_det") }}
-    </b-popover>
-    <swap-limit v-if="limit" />
-    <swap-market v-else />
-  </content-block>
+  <div>
+    <content-block :no-header="true" class="mb-3" :shadow="true">
+      <b-button-group class="d-flex w-100 text-center mt-2 mb-3 pt-3">
+        <b-button
+          id="market"
+          @click="limit = false"
+          :variant="
+            limit ? 'outline-primary' + (darkMode ? '-dark' : '') : 'primary'
+          "
+          >{{ $t("market") }}
+        </b-button>
+        <b-button
+          id="limit"
+          @click="limit = true"
+          :variant="
+            !limit ? 'outline-primary' + (darkMode ? '-dark' : '') : 'primary'
+          "
+          >{{ $t("limit") }}
+        </b-button>
+      </b-button-group>
+      <b-popover
+        target="market"
+        triggers="hover"
+        placement="left"
+        class="font-size-12 font-w400"
+        :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
+      >
+        {{ $t("exe_market_rate") }}
+      </b-popover>
+      <b-popover
+        target="limit"
+        triggers="hover"
+        placement="right"
+        class="font-size-12 font-w400"
+        :class="darkMode ? 'text-muted-dark' : 'text-muted-light'"
+      >
+        {{ $t("trade_at_pre_det") }}
+      </b-popover>
+      <swap-limit v-if="limit" />
+      <swap-market v-else />
+    </content-block>
+    <div class="beta">
+      <img
+        @click="openUrl(`https://beta.bancor.network`)"
+        :src="require(`@/assets/media/logos/beta.png`)"
+        class="mt-1"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -57,6 +66,10 @@ import SwapLimit from "@/components/swap/SwapLimit.vue";
 })
 export default class SwapHome extends BaseComponent {
   limit = false;
+
+  openUrl(url: string) {
+    window.open(url, "_blank");
+  }
 }
 </script>
 
