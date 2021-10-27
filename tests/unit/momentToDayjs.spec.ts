e989e9457af719d3dd0558b7aed6ad1ee36529a8
Ran Cohen <ran@bancor.network>
2021-10-27 11:31:59 +0300

    fixed syntax error

--- tests/unit/momentToDayjs.spec.ts
+++ tests/unit/momentToDayjs.spec.ts
@@ -1,14 +1,15 @@
 //run `yarn test:unit -i momentToDayjs.spec.ts`
 
 // import moment from "moment";
-import dayjs from "@/utils/dayjs"
+import dayjs from "@/utils/dayjs";
 
-describe("check moment and dayjs", () => {  
+describe("check moment and dayjs", () => {
   test("dayjs", () => {
-    const unixTime = 1610107544
-    const oneDayD = dayjs.duration(1, "day")
-    const yesterDayD = dayjs().subtract(oneDayD)
+    const unixTime = 1610107544;
+    const oneDayD = dayjs.duration(1, "day");
+    const yesterDayD = dayjs().subtract(oneDayD);
 
-    expect(dayjs.unix(unixTime).isBefore(dayjs())).toBeTruthy()
-    expect(dayjs().isSameOrBefore(yesterDayD)).toBeFalsy()
-  })
+    expect(dayjs.unix(unixTime).isBefore(dayjs())).toBeTruthy();
+    expect(dayjs().isSameOrBefore(yesterDayD)).toBeFalsy();
+  });
+});
